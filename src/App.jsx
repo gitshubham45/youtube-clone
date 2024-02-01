abfacc1b08433235c397a1f875147b32d73a52d1
gitshubham45 <shubham16290@gmail.com>
2024-02-02 01:44:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AppContext } from './context/contextApi';
+
+const App = () => {
+
+    return (
+        <AppContext>
+            <div className='text-3xl justify-center'>
+                App
+            </div>
+        </AppContext>
+    )
+}
+
+export default App
---
+++ src/context/contextApi.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from "react";
+
+import { fetchDataFromApi } from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(false);
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategory);
+    }, [selectedCategory]);
+
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+            console.log(contents);
+            console.log(process.env.REACT_APP_YOUTUBE_API_KEY);
+            setSearchResults(contents);
+            setLoading(false);
+        });
+    };
+
+    return (
+        <Context.Provider 
+            value={{
+                loading,
+                setLoading,
+                searchResults,
+                selectedCategory,
+                setSelectedCategory,
+                mobileMenu,
+                setMobileMenu
+            }}
+        >
+            {props.children}
+        </Context.Provider>
+    )
+
+}
