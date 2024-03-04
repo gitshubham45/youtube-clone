afaa5b8e91a269e70a1ca0ddee0ec5ff2a2b1f42
gitshubham45 <shubham16290@gmail.com>
2024-03-04 23:39:15 +0530

    vercel analytics added and app key changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import { AppContext } from './context/contextApi';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 import Feed from './components/Feed';
 import Header from './components/Header';
-import LeftNav from './components/LeftNav';
-import LeftNavMenuItem from './components/LeftNavMenuItem';
 import SearchResult from './components/SearchResult';
-import SearchResultVideoCard from './components/SearchResultVideoCard';
-import SuggestionVideoCard from './components/SuggestionVideoCard';
-import VideoCard from './components/VideoCard';
 import VideoDetails from './components/VideoDetails';
 
 
 const App = () => {
-    // console.log(process.env.REACT_APP_YOUTUBE_API_KEY);
 
     return (
         <AppContext>
             <BrowserRouter>
                 <div className='flex  flex-col h-full'>
                     <Header />
+                    <Analytics />
                     <Routes>
                         
                         <Route path="/" exact element={ <Feed />} />
                         <Route path="searchResult/:searchQuery" element={<SearchResult />} />
                         <Route path="/video/:id" element={ <VideoDetails />} />
+                        
 
                     </Routes>
                 </div>
